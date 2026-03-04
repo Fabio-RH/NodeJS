aa802601f10c1d82288e9009882c44445db12427
Fabio <binhorh@hotmail.com>
2026-03-04 15:29:18 -0300

    SelecaoController ajustado

--- src/app.js
+++ src/app.js
@@ -45,60 +45,63 @@ app.get('/selecoes', SelecaoController.index)
 
 
 
-app.get('/selecoes/:id',(req,res)=>{
+app.get('/selecoes/:id', SelecaoController.show)
     //let index = req.params.id
     //console.log(index)
     //res.json(buscarSelecaoPorId(req.params.id))
-        const id = req.params.id
-        const sql = "SELECT * FROM  dbselecao.bdcopa WHERE idselecao=?;"
-    conexao.query(sql, id, (erro, resultado)=>{
-        const linha = resultado[0]
-        if (erro) {
-            console.log(erro)
-            //TODO: STATUS 404 ERROR
-        } else {
-            res.status(200).json(linha)
-        }
-    })
-})
+//         const id = req.params.id
+//         const sql = "SELECT * FROM  dbselecao.bdcopa WHERE idselecao=?;"
+//     conexao.query(sql, id, (erro, resultado)=>{
+//         const linha = resultado[0]
+//         if (erro) {
+//             console.log(erro)
+//             //TODO: STATUS 404 ERROR
+//         } else {
+//             res.status(200).json(linha)
+//         }
+//     })
+// })
 
-app.post('/selecoes',(req,res)=>{
 
+
+app.post('/selecoes', SelecaoController.store)
     //selecoes.push(req.body)
-    const selecao = req.body
-    const sql = "INSERT INTO dbselecao.bdcopa SET ?;"
-    conexao.query(sql, selecao, (erro, resultado)=>{
-        if (erro) {
-            console.log(erro)
-            //TODO: STATUS 404 ERROR
-        } else {
-            res.status(201).json(resultado)
-        }
-    })
+//     const selecao = req.body
+//     const sql = "INSERT INTO dbselecao.bdcopa SET ?;"
+//     conexao.query(sql, selecao, (erro, resultado)=>{
+//         if (erro) {
+//             console.log(erro)
+//             //TODO: STATUS 404 ERROR
+//         } else {
+//             res.status(201).json(resultado)
+//         }
+//     })
 
-})
+// })
 
-app.delete('/selecoes/:id',(req,res)=>{
+app.delete('/selecoes/:id',SelecaoController.delete)
+
     //let index = req.params.id
     //console.log(index)
     //let index = buscarIndexSelecao(req.params.id)
     //selecoes.splice(index,1)
-    const id = req.params.id
-        const sql = "DELETE FROM dbselecao.bdcopa WHERE idselecao=?;"
-    conexao.query(sql, id, (erro, resultado)=>{
-        const linha = resultado[0]
-        if (erro) {
-            console.log(erro)
-            //TODO: STATUS 404 ERROR
-        } else {
-            res.status(200).json(linha)
-        }
-    })
-    res.status(200).json(resultado)
+    // const id = req.params.id
+    //     const sql = "DELETE FROM dbselecao.bdcopa WHERE idselecao=?;"
+    // conexao.query(sql, id, (erro, resultado)=>{
+    //     const linha = resultado[0]
+    //     if (erro) {
+    //         console.log(erro)
+    //         //TODO: STATUS 404 ERROR
+    //     } else {
+    //         res.status(200).json(linha)
+    //     }
+    // })
+    // res.status(200).json(resultado)
     
-})
 
-app.put('/selecoes/:id', (req, res) => {
+
+app.put('/selecoes/:id', SelecaoController.update)
+
     //let index = buscarIndexSelecao(req.params.id)
 
     //if (index === -1) {
@@ -109,23 +112,25 @@ app.put('/selecoes/:id', (req, res) => {
         //id: selecoes[index].id,
         //...req.body
     //}
-        const id = req.params.id
-        const selecao = req.body
-        const sql = "UPDATE dbselecao.bdcopa SET ? WHERE idselecao ? ;"
-        console.log (id)
-        console.log (selecao)
-        console.log (sql)
-    conexao.query(sql, [selecao,id], (erro, resultado)=>{
-        if (erro) {
-            console.log(erro)
-            //TODO: STATUS 404 ERROR
-        } else {
-            res.status(200).json(resultado)
-        }
-    })
+    // const id = req.params.id
+    // const selecao = req.body
+    // const sql = "UPDATE dbselecao.bdcopa SET ? WHERE idselecao = ?;"
 
-    res.send("Seleção atualizada com sucesso!")
-})
+    // console.log(id)
+    // console.log(selecao)
+
+    // conexao.query(sql, [selecao, id], (erro, resultado) => {
+    //     if (erro) {
+    //         console.log(erro)
+    //         return res.status(500).json(erro)
+    //     }
+
+    //     res.status(200).json({
+    //         mensagem: "Seleção atualizada com sucesso!",
+    //         resultado
+    //     })
+    // })
+
 
 
 export default app
--- src/app/controllers/SelecaoController.js
+++ src/app/controllers/SelecaoController.js
@@ -15,16 +15,73 @@ class SelecaoController {
     }
 
     // LISTAR POR id
-    show() {}
+    show(req,res) {const id = req.params.id
+        const sql = "SELECT * FROM  dbselecao.bdcopa WHERE idselecao=?;"
+    conexao.query(sql, id, (erro, resultado)=>{
+        const linha = resultado[0]
+        if (erro) {
+            console.log(erro)
+            //TODO: STATUS 404 ERROR
+        } else {
+            res.status(200).json(linha)
+        }
+    })
+
+
+    }
 
     // Criar dados
-    store () {}
+    store (req,res) {
+        const selecao = req.body
+    const sql = "INSERT INTO dbselecao.bdcopa SET ?;"
+    conexao.query(sql, selecao, (erro, resultado)=>{
+        if (erro) {
+            console.log(erro)
+            //TODO: STATUS 404 ERROR
+        } else {
+            res.status(201).json(resultado)
+        }
+    })
+    }
 
     // Atualizar
-    update(){}
+    update(req,res){
+    const id = req.params.id
+    const selecao = req.body
+    const sql = "UPDATE dbselecao.bdcopa SET ? WHERE idselecao = ?;"
+
+    console.log(id)
+    console.log(selecao)
+
+    conexao.query(sql, [selecao, id], (erro, resultado) => {
+        if (erro) {
+            console.log(erro)
+            return res.status(500).json(erro)
+        }
+
+        res.status(200).json({
+            mensagem: "Seleção atualizada com sucesso!",
+            resultado
+        })
+    })
+        
+    }
 
     //remover dados
-    delete (){}
+    delete (req,res){
+        const id = req.params.id
+        const sql = "DELETE FROM dbselecao.bdcopa WHERE idselecao=?;"
+    conexao.query(sql, id, (erro, resultado)=>{
+        const linha = resultado[0]
+        if (erro) {
+            console.log(erro)
+            //TODO: STATUS 404 ERROR
+        } else {
+            res.status(200).json(linha)
+        }
+    })
+    res.status(200).json(resultado)
+    }
 }
 //padrao singleton
 export default new SelecaoController()
