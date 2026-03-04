cc4ee00197383cca9a0c47487a1565e5115cc69a
Fabio-RH <binhorh@hotmail.com>
2026-03-03 21:57:19 -0300

    QUARTA AULA

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 import app from './src/app.js'
-import conexao from './infra/conexao.js'
+// import conexao from './infra/conexao.js'
 const port = 3000
 
 //realizando a conexão
-conexao.connect((erro) =>{
-    if (erro){
-        console.log("Erro na Conexão",erro)
-    }else {
-        console.log("Conexão Com Sucesso")
-        //escutando a porta
+//conexao.connect((erro) =>{
+//    if (erro){
+//       console.log("Erro na Conexão",erro)
+//   }else {
+//        console.log("Conexão Com Sucesso")
+//        //escutando a porta
         app.listen(port, ()=>{
         console.log(`Servidor rodando http://localhost:${port}`)
 }
-)}
-})
+)
+//}
+//})
