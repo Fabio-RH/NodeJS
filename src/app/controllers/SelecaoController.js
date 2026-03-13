a1f5b773b98050031d28a32642982bcb8b935128
Fabio-RH <binhonh@hotmail.com>
2026-03-13 13:38:09 -0300

    Consertando Repository e Controller

--- src/app/controllers/SelecaoController.js
+++ src/app/controllers/SelecaoController.js
@@ -1,118 +1,58 @@
-import conexao from '../database/conexao.js'
+import SelecaoRepository from '../repositories/SelecaoRepository.js'
 
 class SelecaoController {
-
     
 
-    // LISTAR TODAS
-    index(req, res) {
 
-        const sql = "SELECT * FROM dbselecao.bdcopa"
-
-        conexao.query(sql, (erro, resultado) => {
-
-            if (erro) {
-                console.log(erro)
-                return res.status(500).json({ erro: "Erro ao buscar seleções" })
-            }
-
+    async index(req, res) {
+        try {
+            const resultado = await SelecaoRepository.findAll()
             res.status(200).json(resultado)
-
-        })
-
+        } catch (erro) {
+            res.status(404).json({ 'erro': erro })
+        }
     }
 
-
-    // LISTAR POR ID
-    show(req, res) {
-
-        const id = req.params.id
-        const sql = "SELECT * FROM dbselecao.bdcopa WHERE idselecao = ?"
-
-        conexao.query(sql, id, (erro, resultado) => {
-
-            if (erro) {
-                console.log(erro)
-                return res.status(500).json({ erro: "Erro ao buscar seleção" })
-            }
-
+    async show(req, res) {
+        try {
+            const id = req.params.id
+            const resultado = await SelecaoRepository.findById(id)
             res.status(200).json(resultado[0])
-
-        })
-
+        } catch (erro) {
+            res.status(404).json({ 'erro': erro })
+        }
     }
 
-
-    // CRIAR
-    store(req, res) {
-
-        const selecao = req.body
-        const sql = "INSERT INTO dbselecao.bdcopa SET ?"
-
-        conexao.query(sql, selecao, (erro, resultado) => {
-
-            if (erro) {
-                console.log(erro)
-                return res.status(500).json({ erro: "Erro ao cadastrar seleção" })
-            }
-
-            res.status(201).json({
-                mensagem: "Seleção cadastrada com sucesso",
-                resultado
-            })
-
-        })
-
+    async store(req, res) {
+        try {
+            const selecao = req.body
+            const resultado = await SelecaoRepository.create(selecao)
+            res.status(201).json(resultado)
+        } catch (erro) {
+            res.status(400).json({ 'erro': erro })
+        }
     }
 
-
-    // ATUALIZAR
-    update(req, res) {
-
-        const id = req.params.id
-        const selecao = req.body
-
-        const sql = "UPDATE dbselecao.bdcopa SET ? WHERE idselecao = ?"
-
-        conexao.query(sql, [selecao, id], (erro, resultado) => {
-
-            if (erro) {
-                console.log(erro)
-                return res.status(500).json({ erro: "Erro ao atualizar seleção" })
-            }
-
-            res.status(200).json({
-                mensagem: "Seleção atualizada com sucesso",
-                resultado
-            })
-
-        })
-
+    async update(req, res) {
+        try {
+            const id = req.params.id
+            const selecao = req.body
+            const resultado = await SelecaoRepository.update(selecao, id)
+            res.status(200).json(resultado)
+        } catch (erro) {
+            res.status(400).json({ 'erro': erro })
+        }
     }
 
-
-    // DELETAR
-    delete(req, res) {
-
-        const id = req.params.id
-        const sql = "DELETE FROM dbselecao.bdcopa WHERE idselecao = ?"
-
-        conexao.query(sql, id, (erro, resultado) => {
-
-            if (erro) {
-                console.log(erro)
-                return res.status(500).json({ erro: "Erro ao deletar seleção" })
-            }
-
-            res.status(200).json({
-                mensagem: "Seleção removida com sucesso",
-                resultado
-            })
-
-        })
-
+    async delete(req, res) {
+        try {
+            const id = req.params.id
+            const resultado = await SelecaoRepository.delete(id)
+            res.status(200).json(resultado)
+        } catch (erro) {
+            res.status(404).json({ 'erro': erro })
+        }
     }
-
 }
 
 export default new SelecaoController()
--- src/app/repositories/SelecaoRepository.js
+++ src/app/repositories/SelecaoRepository.js
@@ -1,22 +1,39 @@
-import conexao from "../database/conexao.js"
+class SelecaoRepository {
 
-
-class SelecaoRepository{
-    create(){}
-
-    findAll(){
-        const sql = "SELECT * FROM dbselecao.dbcopa "
-        return new Promise ((resolve,reject)=>{
-            conexao.query(sql, (erro,resultado)=>{
-                if (erro){
-                    return reject(erro)
-                } else {
-                    const row = JSON.parse(JSON.stringify(resultado))
-                    return resolve (row)
-                }
+    // método pra deixar as queries centralizado no banco
+    executarQuery(sql, valores = '', mensagemErro = 'Erro ao realizar a operação') {
+        return new Promise((resolve, reject) => {
+            conexao.query(sql, valores, (erro, resultado) => {
+                if (erro) return reject(mensagemErro)
+                return resolve(resultado)
             })
         })
+    }
 
+    findAll() {
+        const sql = "SELECT * FROM dbselecao.bdcopa;"
+        return this.executarQuery(sql, 'Não foi possível listar')
+    }
+
+    findById(id) {
+        const sql = "SELECT * FROM dbselecao.bdcopa WHERE id=?;"
+        return this.executarQuery(sql, id, 'Não foi possível localizar')
+    }
+
+    create(selecao) {
+        const sql = "INSERT INTO dbselecao.bdcopa SET ?;"
+        return this.executarQuery(sql, selecao, 'Não foi possível cadastrar')
+    }
+
+    update(selecao, id) {
+        const sql = "UPDATE dbselecao.bdcopa SET ? WHERE id=?;"
+        return this.executarQuery(sql, [selecao, id], 'Não foi possível atualizar')
+    }
+
+    delete(id) {
+        const sql = "DELETE FROM dbselecao.bdcopa WHERE id=?;"
+        return this.executarQuery(sql, id, 'Não foi possível apagar')
+    }
 }
 
-}
+export default new SelecaoRepository()
